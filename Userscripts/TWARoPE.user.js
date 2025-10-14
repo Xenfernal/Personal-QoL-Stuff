6bab2589cd6c81df7fbf8999b937b760fb4ac59f
Xen <65840847+Xenfernal@users.noreply.github.com>
2025-10-14 15:14:54 +0500

    Update TWARoPE.user.js

--- Userscripts/TWARoPE.user.js
+++ Userscripts/TWARoPE.user.js
@@ -1,13 +1,16 @@
 // ==UserScript==
 // @name         Twitch Auto Reload on Player Errors (God Mode)
-// @namespace    Xen
+// @namespace    https://github.com/Xenfernal
 // @version      5.3
 // @icon         https://static.twitchcdn.net/assets/favicon-32-e29e246c157142c94346.png
 // @description  Reload Twitch stream if any player error appears. Full feature set: GUI, retry counter, audio, notifications, logs, dark mode, draggable panel, and error history tracking. Improved script made originally by SNOOKEE.
 // @author       Xen
 // @match        https://www.twitch.tv/*
 // @grant        GM_registerMenuCommand
-// @license MIT
+// @license      MIT
+// @homepageURL  https://github.com/Xenfernal/Personal-QoL-Stuff/tree/main/Userscripts
+// @downloadURL  https://github.com/Xenfernal/Personal-QoL-Stuff/raw/refs/heads/main/Userscripts/TWARoPE.user.js
+// @updateURL    https://github.com/Xenfernal/Personal-QoL-Stuff/raw/refs/heads/main/Userscripts/TWARoPE.user.js
 // ==/UserScript==
 
 (function () {
